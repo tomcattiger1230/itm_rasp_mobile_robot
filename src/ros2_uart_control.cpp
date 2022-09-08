53271c2f7af6c5eb7e5f400d3a19f0ccdf06a27e
Wei Luo <wei.luo.88@outlook.com>
2022-09-08 14:32:34 +0200

    create a ROS2 package for Respberry Pi on mobile robot

---
+++ src/ros2_uart_control.cpp
@@ -0,0 +1,7 @@
+/*
+ * @Author: Wei Luo
+ * @Date: 2022-09-08 14:31:07
+ * @LastEditors: Wei Luo
+ * @LastEditTime: 2022-09-08 14:31:08
+ * @Note: this cpp is to set up UART communication with BeagleBone onboard computer
+ */
