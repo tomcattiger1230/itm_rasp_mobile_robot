0fae2bb39bdeea1425cb65498e0d8568e8019f8e
Wei Luo <wei.luo.88@outlook.com>
2022-11-28 10:21:40 +0100

    update readme and code description

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # itm_rasp_mobile_robot
+
+This package is for controlling the ITM mobile robots on a Raspberry Pi onboard computer with Ubuntu OS. This branch is available for ROS 2, if you want to use the codes for ROS 1. Please change to the **ros1** branch.
+
+## Communicating through UART
+
+Based on the work from Dr. Ebel and Mr. Rosenfelder, one can directly communicate with the onboard computer Beaglebone on the ITM mobile robots through UART. The ROS commands can be transferred through the UART without any special packages on the Beaglebone. One can launch the program
+
+```bash
+ros2 launch itm_rasp_mobile_robot cmd_to_serial.launch.py
+```
+
+Two arguments in the configure file **config/ros_cmd_uart.yaml** need to be specified:
+
+- port: which UART port of the Raspberry Pi is to be used. Note that, in some cases, the UART port should be authorized.
+- sub_topic: subscribed topic in ROS, which is to be transferred through the UART connection.
+
+
+## Open the onboard USB Camera
+
+In the practice of **HERA Motion** one may need to subscribe the message from camera. The configuration are specified in the configure file in the folder **config**.
+
+For instance, you can launch a camera node
+
+```bash
+ros2 launch itm_rasp_mobile_robot usb_cam.launch.py
+```
+
+Note that in the **config** folder there are several configure files for camera experiments. One can use **usb_cam** package to control the USB camera that is attached on the Raspberry Pi.
--- src/ros2_uart_control.cpp
+++ src/ros2_uart_control.cpp
@@ -2,7 +2,7 @@
  * @Author: Wei Luo
  * @Date: 2022-09-08 14:31:07
  * @LastEditors: Wei Luo
- * @LastEditTime: 2022-11-23 17:17:01
+ * @LastEditTime: 2022-11-28 10:20:11
  * @Note: this cpp is to set up UART communication with BeagleBone onboard
  * computer
  */
@@ -12,6 +12,7 @@
 UartControl::UartControl()
     : Node("uart_control_node"), is_initialized(false),
       got_velocity_cmd(false) {
+  /* declare several ros parameters */
   this->declare_parameter<std::string>("port", "/dev/ttyS0");
   this->get_parameter("port", port_name_);
 
@@ -21,6 +22,8 @@ UartControl::UartControl()
   this->declare_parameter<std::string>("ros_cmd_topic", "/robot_cmd");
   this->get_parameter("ros_cmd_topic", cmd_topic_name);
 
+  /* This is a subscription to the topic `/robot_cmd` and the callback function is
+  `robot_velocity_cmd_callback`. */
   cmd_sub_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
       cmd_topic_name, 10,
       std::bind(&UartControl::robot_velocity_cmd_callback, this,
@@ -28,6 +31,7 @@ UartControl::UartControl()
   RCLCPP_INFO(this->get_logger(), "robot gets ros2 topic is: %s",
               cmd_topic_name.c_str());
 
+  /* This is to set up the serial port. */
   serial::Timeout to = serial::Timeout::simpleTimeout(100);
   sp.setPort(port_name_);
   sp.setBaudrate(baud_rate);
@@ -48,6 +52,11 @@ UartControl::UartControl()
 
 UartControl::~UartControl() {}
 
+/**
+ * It takes the velocity command from the ROS topic and stores it in the array `received_cmd`
+ *
+ * @param msg The message that was received.
+ */
 void UartControl::robot_velocity_cmd_callback(
     const geometry_msgs::msg::TwistStamped::SharedPtr msg) {
 
@@ -60,6 +69,9 @@ void UartControl::robot_velocity_cmd_callback(
   received_cmd[2] = msg->twist.angular.z;
 }
 
+/**
+ * It checks if a new velocity command has been received, and if so, it sends it to the Raspberry Pi
+ */
 void UartControl::run() {
   if (got_velocity_cmd) {
     std::string cmd_string_;
@@ -74,6 +86,7 @@ void UartControl::run() {
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
+  /* To set the frequency of the loop. */
   rclcpp::Rate rate(50.0);
 
   auto uart_control_handle = std::make_shared<UartControl>();
